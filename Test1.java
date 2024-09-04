33ce49551b5d44f3ef61c86295167241d1e3f1eb
Anjana Suchithra Babu(UST <284288@ust.com>
2024-09-04 12:56:07 +0530

    Testing

---
+++ Test1.java
@@ -0,0 +1,19 @@
+package com.ust.TestingGit;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Sum: " + addition(10, 20));
+		System.out.println("Difference: " + subtration(30, 10));
+	}
+
+	public static int addition(int a, int b) {
+		return a + b;
+	}
+
+	public static int subtration(int a, int b) {
+		return a - b;
+	}
+
+}
